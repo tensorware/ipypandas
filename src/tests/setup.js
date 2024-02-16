6f8c2bbfa9ffd41ff8eed34a0fa6c625a1b08577
Francis Seits <francis.seits@tensorware.net>
2024-02-16 23:38:09 +0100

    ref: update jest config

--- src/tests/setup.js
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Tensorware.
-// Distributed under the terms of the Modified BSD License.
-
-// polyfill for drag event
-Object.defineProperty(window, 'DragEvent', {
-    value: class DragEvent {}
-});
