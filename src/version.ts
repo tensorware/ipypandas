fb33c1d8725a8197fe5d3c6290c17ba5b548037d
Francis Seits <francis.seits@tensorware.net>
2024-02-19 20:00:57 +0100

    ref: remove typescript command

--- src/version.ts
+++ src/version.ts
@@ -1,9 +1,8 @@
 // Copyright (c) Tensorware.
 // Distributed under the terms of the Modified BSD License.
 
-// @ts-ignore
-const pkg = require('../package.json');
+import { name, version } from '../package.json';
 
-export const MODULE_NAME = pkg.name;
-export const MODULE_SEMVER = `^${pkg.version}`;
-export const MODULE_VERSION = pkg.version;
+export const MODULE_NAME = name;
+export const MODULE_SEMVER = `^${version}`;
+export const MODULE_VERSION = version;
